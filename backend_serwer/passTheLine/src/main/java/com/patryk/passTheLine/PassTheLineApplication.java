7ef842afca21e4057fc5c029310d4b9f8cf1dd40
UmarlyPoeta <pkozlowski@student.agh.edu.pl>
2025-10-04 20:27:51 +0200

    Initialize Spring Boot project with Maven wrapper and basic structure
    - Added .gitattributes and .gitignore files for project configuration.
    - Included Maven wrapper properties and scripts for easier Maven management.
    - Created pom.xml with Spring Boot dependencies and configurations.
    - Implemented main application class PassTheLineApplication.
    - Added application.properties for basic application settings.
    - Created test class PassTheLineApplicationTests for context loading verification.

---
+++ backend_serwer/passTheLine/src/main/java/com/patryk/passTheLine/PassTheLineApplication.java
@@ -0,0 +1,13 @@
+package com.patryk.passTheLine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassTheLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassTheLineApplication.class, args);
+	}
+
+}
---
+++ backend_serwer/passTheLine/src/test/java/com/patryk/passTheLine/PassTheLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patryk.passTheLine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassTheLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
